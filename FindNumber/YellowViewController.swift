8e1918bb2d4c81a76703b185a7f6dcdba8409edd
Boris <ea.foreverrr@gmail.com>
2022-03-10 23:34:31 +0300

    ViewController lifecycle

--- FindNumber/YellowViewController.swift
+++ FindNumber/YellowViewController.swift
@@ -11,9 +11,36 @@ class YellowViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+        print("YellowViewController -> viewDidLoad")
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("YellowViewController -> viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("YellowViewController -> viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("YellowViewController -> viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("YellowViewController -> viewDidDisappear")
+    }
+    
+    // deinit вызывается тогда когда уничтожается экземпляр класса и на него нету strong ссылок
+    
+    deinit{
+        print("YellowViewController -> deinit")
+    }
+    
+    // -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
 
     @IBAction func OpenBlueScreen(_ sender: Any) {
         performSegue(withIdentifier: "goToBlue", sender: nil)
